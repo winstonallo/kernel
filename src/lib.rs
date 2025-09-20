1d4d27fe41a395b716d4953c0170fe91a719cb00
winstonallo <arthurbiedchar@gmail.com>
2025-09-20 21:32:31 +0200

    Init IDT and add division by zero to test interrupt handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #![no_std]
 #![cfg_attr(test, no_main)]
+#![feature(abi_x86_interrupt)]
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner::test_runner)]
 #![reexport_test_harness_main = "test_main"]
@@ -7,12 +8,17 @@
 #[cfg(test)]
 use core::panic::PanicInfo;
 
+pub mod interrupts;
 pub mod macros;
 pub mod port;
 pub mod qemu;
 pub mod serial;
 pub mod test_runner;
 pub mod vga_buffer;
+
+pub fn init() {
+    interrupts::init_idt();
+}
 
 #[cfg(test)]
 #[panic_handler]
@@ -23,6 +29,7 @@ fn panic(info: &PanicInfo) -> ! {
 #[cfg(test)]
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
+    init();
     test_main();
     loop {}
 }
--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,27 @@
 #![no_main]
 #![reexport_test_harness_main = "test_main"]
 
-use kernel::printkln;
+use kernel::{interrupts, printkln};
 mod panic;
+
+fn divide_by_zero() {
+    unsafe { core::arch::asm!("mov dx, 0; div dx") }
+}
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
     printkln!("Hello, World{}", "!");
+
+    kernel::init();
+
     #[cfg(test)]
     test_main();
+
+    interrupts::init_idt();
+
+    divide_by_zero();
+
+    printkln!("It did not crash");
     #[allow(clippy::empty_loop)]
     loop {}
 }
