2b6ccded90b1fe160cdf760d8f83ed9f06c4e09f
winstonallo <arthurbiedchar@gmail.com>
2025-09-20 21:32:01 +0200

    Add IDT with division by zero handler

---
+++ src/interrupts.rs
@@ -0,0 +1,21 @@
+use lazy_static::lazy_static;
+
+mod idt;
+use crate::{interrupts::idt::InterruptDescriptorTable, printkln};
+
+lazy_static! {
+    static ref IDT: InterruptDescriptorTable = {
+        let mut idt = InterruptDescriptorTable::new();
+        idt.set_handler(0, divide_by_zero_handler);
+        idt
+    };
+}
+
+pub fn init_idt() {
+    IDT.load();
+}
+
+extern "C" fn divide_by_zero_handler() -> ! {
+    printkln!("EXCEPTION: DIVIDE BY ZERO");
+    loop {}
+}
---
+++ src/interrupts/idt.rs
@@ -0,0 +1,101 @@
+use x86_64::instructions::segmentation;
+use x86_64::registers::segmentation::Segment;
+use x86_64::structures::gdt::SegmentSelector;
+use x86_64::{PrivilegeLevel, VirtAddr};
+
+pub type HandlerFunc = extern "C" fn() -> !;
+pub struct InterruptDescriptorTable([Entry; 16]);
+
+impl InterruptDescriptorTable {
+    pub fn new() -> Self {
+        Self([Entry::missing(); 16])
+    }
+
+    pub fn set_handler(&mut self, entry: u8, handler: HandlerFunc) -> &mut EntryOptions {
+        self.0[entry as usize] = Entry::new(segmentation::CS::get_reg(), handler);
+        &mut self.0[entry as usize].options
+    }
+
+    pub fn load(&'static self) {
+        use x86_64::instructions::tables::{DescriptorTablePointer, lidt};
+
+        let ptr = DescriptorTablePointer {
+            base: VirtAddr::new(self as *const _ as u64),
+            limit: (core::mem::size_of::<Self>() - 1) as u16,
+        };
+
+        unsafe { lidt(&ptr) };
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+#[repr(C, align(16))]
+pub struct Entry {
+    pointer_low: u16,
+    gdt_selector: SegmentSelector,
+    options: EntryOptions,
+    pointer_middle: u16,
+    pointer_high: u32,
+    reserved: u32,
+}
+
+impl Entry {
+    fn new(gdt_selector: SegmentSelector, handler: HandlerFunc) -> Self {
+        let pointer = handler as usize;
+        Entry {
+            gdt_selector,
+            pointer_low: pointer as u16,
+            pointer_middle: (pointer >> 16) as u16,
+            pointer_high: (pointer >> 32) as u32,
+            options: EntryOptions::new(),
+            reserved: 0,
+        }
+    }
+
+    fn missing() -> Self {
+        Entry {
+            gdt_selector: SegmentSelector::new(0, PrivilegeLevel::Ring0),
+            pointer_low: 0,
+            pointer_middle: 0,
+            pointer_high: 0,
+            options: EntryOptions::minimal(),
+            reserved: 0,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct EntryOptions(u16);
+
+impl EntryOptions {
+    fn new() -> Self {
+        Self::minimal()
+            .set_present(true)
+            .disable_interrupts(true)
+            .clone()
+    }
+
+    fn minimal() -> Self {
+        Self(0b111 << 9)
+    }
+
+    pub fn set_present(&mut self, present: bool) -> &mut Self {
+        self.0 |= (present as u16) << 15;
+        self
+    }
+
+    pub fn disable_interrupts(&mut self, disable: bool) -> &mut Self {
+        self.0 |= (!disable as u16) << 8;
+        self
+    }
+
+    pub fn set_privilege_level(&mut self, dpl: u16) -> &mut Self {
+        self.0 = (self.0 & !(0b11 << 13)) | ((dpl & 0b11) << 13);
+        self
+    }
+
+    pub fn set_stack_index(&mut self, index: u16) -> &mut Self {
+        self.0 = ((self.0 >> 3) << 3) | index;
+        self
+    }
+}
