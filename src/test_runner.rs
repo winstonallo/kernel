6137c474da021970d096d3ff3e8766a990a3763c
winstonallo <arthurbiedchar@gmail.com>
2025-09-20 15:17:32 +0200

    Add test_runner.rs with Testable trait

---
+++ src/test_runner.rs
@@ -0,0 +1,37 @@
+use core::panic::PanicInfo;
+
+use crate::{serial_print, serial_println};
+
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) -> () {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
+
+pub fn panic_handler(info: &PanicInfo) -> ! {
+    use crate::qemu::{self, exit};
+
+    serial_println!("[failed]");
+    serial_println!("Error: {}", info);
+    exit(qemu::ExitCode::Failure);
+    loop {}
+}
+
+pub fn test_runner(tests: &[&dyn Testable]) {
+    use crate::qemu;
+
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test.run();
+    }
+    qemu::exit(qemu::ExitCode::Success);
+}
