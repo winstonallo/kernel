e6eb06427362e6010e7851dbacd2e2584d723f00
winstonallo <arthurbiedchar@gmail.com>
2025-09-28 16:56:18 +0200

    Fix clippy errors

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,9 +1,38 @@
 use lazy_static::lazy_static;
 
 mod idt;
-use crate::{interrupts::idt::InterruptDescriptorTable, printkln};
+use crate::{address::VirtualAddress, interrupts::idt::InterruptDescriptorTable, printkln, registers::SegmentSelector};
 
 pub use idt::InterruptDescriptorTablePointer;
+
+#[allow(unused)]
+pub struct CPUFlags(usize);
+
+impl CPUFlags {
+    pub fn new() -> Self {
+        Self(0)
+    }
+}
+
+impl Default for CPUFlags {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+pub struct InterruptStackFrame {
+    pub instruction_pointer: VirtualAddress,
+    pub code_segment: SegmentSelector,
+    _reserved: [u8; 6],
+    pub cpu_flags: usize, // wrong
+    pub stack_pointer: VirtualAddress,
+    pub stack_segment: SegmentSelector,
+    _reserved2: [u8; 6],
+}
+
+impl InterruptStackFrame {
+    // pub fn new(inst)
+}
 
 lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
@@ -22,3 +51,5 @@ extern "C" fn divide_by_zero_handler() -> ! {
     #[allow(clippy::empty_loop)]
     loop {}
 }
+
+// extern "C" fn breakpoint_handler(stack_frame: InterruptStackFrame) {}
