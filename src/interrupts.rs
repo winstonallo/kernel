b7510b911bae54b76b92a811e943d9ed8ddfd79b
winstonallo <arthurbiedchar@gmail.com>
2025-10-30 11:39:28 +0100

    Fix CLippy errors

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -80,12 +80,12 @@ extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: InterruptStac
     let mut port = Port::new(0x60);
 
     let scancode: u8 = unsafe { port.read() };
-    if let Ok(Some(key_event)) = keyboard.add_byte(scancode) {
-        if let Some(key) = keyboard.process_keyevent(key_event) {
-            match key {
-                DecodedKey::Unicode(character) => printk!("{}", character),
-                DecodedKey::RawKey(key) => printk!("{:?}", key),
-            }
+    if let Ok(Some(key_event)) = keyboard.add_byte(scancode)
+        && let Some(key) = keyboard.process_keyevent(key_event)
+    {
+        match key {
+            DecodedKey::Unicode(character) => printk!("{}", character),
+            DecodedKey::RawKey(key) => printk!("{:?}", key),
         }
     }
 
